5fbf160c5f767094b4b102d2b832672698dd2556
kevtat <kevvtat@gmail.com>
2023-03-01 14:28:57 -0800

    top 3

--- BG2/code.R
+++ BG2/code.R
@@ -1,1 +1,13 @@
-da
+
+
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+df <- read_delim("world_population.csv")
+
+df %>%
+  group_by(Continent) %>%
+  arrange(`Growth Rate`) %>%
+  slice_head(n = 10) %>%
+  select(Continent, `Country/Territory`)
+
