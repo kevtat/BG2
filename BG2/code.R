30da6a344de822abd74441d67d678defefd0dafa
kevtat <kevvtat@gmail.com>
2023-03-02 22:18:13 -0800

    working on growth rate

--- BG2/code.R
+++ BG2/code.R
@@ -4,10 +4,22 @@ library(ggplot2)
 library(tidyverse)
 library(dplyr)
 df <- read_delim("world_population.csv")
-
+#highest
 df %>%
   group_by(Continent) %>%
-  arrange(`Growth Rate`) %>%
-  slice_head(n = 10) %>%
+  arrange(desc(`Growth Rate`)) %>%
+  slice_head(n = 1) %>%
   select(Continent, `Country/Territory`)
+
+
+#lowest
+df %>%
+  group_by(Continent) %>%
+  arrange(desc(`Growth Rate`)) %>%
+  slice_tail(n = 1) %>%
+  select(Continent, `Country/Territory`)
+
+df %>% 
+  group_by(Continent) %>% 
+  summarise(Averagegrowthrate = mean(`Growth Rate`))
 
